ab00749cb421f63e9034c9cd3335581e1c490049
Micheal LeBlanc <Mikeleblanc8489@gmail.com>
2023-03-31 16:44:55 -0400

    Add comments to program

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ Date: 03/31/2023
 
 # Import Libraries
 import string #for random password creation
-import datetime #as a timestamp of when user/pass was create
 import secrets
 import sys
 
@@ -17,6 +16,7 @@ database = []
 
 
 # Define functions
+# The main menu of the program
 def core_iteration():
     print("\n******Welcome to the Unsafe Database******")
     print("Select a number from the Main Menu:")
@@ -43,10 +43,12 @@ def get_user_input(selection):
         case "E":
             exit_system()
 
+# Stops the program on request
 def exit_system():
     print("Thanks for trusting that your information is safe with Unsafe Database!")
     sys.exit()
 
+# Takes username input
 def get_username():
     print("\nEnter a username to be entered into the database.")
     username = input("Username: ")
@@ -61,27 +63,30 @@ def get_username():
     #Back to Main Menu
     core_iteration()
 
+# Checks if a username is already in the database so an entry can be entered
 def check_for_duplicates(check_list, duplicate):
     is_duplicate = False
     #Checks each key at database index
     for dictionary_index in check_list:
         if duplicate in dictionary_index:
             is_duplicate = True
-            # print("This username is taken.")
             return is_duplicate
     return is_duplicate
 
+# Adds the username and pass as a dictionary into the database
 def append_to_database(uname, upass):
     entry = dict()
     entry[uname] = upass
     database.append(entry)
     print(f"{uname}'s password is: {upass}")
 
+# Generates the random password of characters and integers
 def generate_password():
     pass_inputs = string.ascii_letters + string.digits
     password = ''.join(secrets.choice(pass_inputs) for i in range(12))
     return password
 
+# Checks if the username is already in the database
 def lookup_username():
     print("\nEnter the username you are looking for.")
     lookup = input("User:")
@@ -94,6 +99,7 @@ def lookup_username():
     #Back to Main Menu
     core_iteration()
 
+# Determines if the user selection at main menu is a valid option
 def is_input_valid(check_input):
     valid_entries = ["1", "2", "e", "E"]
     if check_input in valid_entries:
